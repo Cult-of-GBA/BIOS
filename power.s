f0834c45ec63575f3059a11eafa955e88b05f0ce
Frederic Meyer <fleroviux@gmail.com>
2020-07-26 22:32:35 +0200

    Derp

--- power.s
+++ power.s
@@ -30,14 +30,13 @@ swi_IntrWait:
     @ IME = 0
     @ If we don't do this the SWI and the IRQ handler will race for the BIOS interrupt flags.
     strb r2, [r12, #(REG_IME - MMIO_BASE)]
-    
+
     @ Read BIOS interrupt flags which are stored at 0x03FFFFF8 and updated by the IRQ handler.
     @ This contains the IRQs that already have been processed by the IRQ handler.
     ldrh r4, [r12, #-8]
 
     @ Extract already processed IRQs that we were waiting for and acknowledge them.
-    and r0, r1, r4
-    cmp r0, #0
+    ands r0, r1, r4
     eorne r4, r0
     strneh r4, [r12, #-8]
 
@@ -47,8 +46,8 @@ swi_IntrWait:
 
 .intr_wait_loop_end:
     @ Repeat if we didn't get any IRQ that we were waiting for.
-    @ This reuses the zero-flag from the previous comparison in .intr_wait_check_processed_irqs.
-    bne .intr_wait_halt_until_irq
+    @ This reuses the zero-flag from the ANDS instruction in .intr_wait_check_processed_irqs.
+    beq .intr_wait_halt_until_irq
 
     ldmfd sp!, {r4, lr}
     bx lr
