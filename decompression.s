3187d1efec5041265fb0203fefa2da7dd207812f
Frederic Meyer <fleroviux@gmail.com>
2020-07-26 20:03:28 +0200

    Move swi_LZ77UnCompWrite8bit to decompression.s

--- decompression.s
+++ decompression.s
@@ -5,6 +5,74 @@
     beq \return_branch
 .endm
 
+@ TODO: check for invalid addresses < 0x02000000!
+swi_LZ77UnCompWrite8bit:
+    stmfd sp!, {r3 - r6}
+
+    @ Read header word:
+    @ bit0-3:  reserved
+    @ bit4-7:  compressed type (1 for LZ77)
+    @ bit8-31: size of compressed data
+    ldr r2, [r0], #4
+    lsrs r2, r2, #8
+    @ ignore zero-length decompression requests
+    beq .lz77_done
+
+.lz77_loop:
+    @ read encoder byte, shift to MSB for easier access.
+    ldrb r3, [r0], #1
+    orr r3, #0x01000000
+.lz77_encoder_loop:
+    tst r3, #0x80
+    bne .lz77_copy_window
+.lz77_copy_byte:
+    @ copy byte from current source to current destination
+    ldrb r4, [r0], #1
+    strb r4, [r1], #1
+
+    @ check if decompressed length has been reached.
+    subs r2, #1
+    beq .lz77_done
+
+    @ read next encoder or process next block
+    lsls r3, #1
+    bcc .lz77_encoder_loop
+    b .lz77_loop
+.lz77_copy_window:
+    @ read window tuple {displacement, size}
+    ldrb r4, [r0], #1
+    ldrb r5, [r0], #1
+
+    @ r5 = window displacement
+    orr r5, r5, r4, lsl #8
+    bic r5, #0xF000
+    add r5, #1
+
+    @ r4 = window size
+    lsr r4, #4
+    add r4, #3
+.lz77_copy_window_loop:
+    @ copy byte from window to current destination
+    ldrb r6, [r1, -r5]
+    strb r6, [r1], #1
+
+    @ check if decompressed length has been reached
+    subs r2, #1
+    beq .lz77_done
+
+    @ check if window has been fully copied
+    subs r4, #1
+    bne .lz77_copy_window_loop
+
+    @ read next encoder or process next block
+    lsls r3, #1
+    bcc .lz77_encoder_loop
+    b .lz77_loop
+
+.lz77_done:
+    ldmfd sp!, {r3 - r6}
+    bx lr
+
 @ I decompiled this from the original BIOS:
 
 @ function BitUnpack(src_addr, dest_addr, src_len, src_width, dest_width, data_offset):
